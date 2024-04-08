c5eb76fa7759032b3f9338c8d9effbd9137c95b2
kat369 <kathiravanarjunan369@gmail.com>
2024-04-08 14:43:44 +0530

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,145 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const datum = [
+    {
+      roomtype: "Single",
+      price: 100,
+      service: 10,
+      available: 5,
+    },
+    {
+      roomtype: "Double",
+      price: 150,
+      service: 15,
+      available: 10,
+    },
+    {
+      roomtype: "Dulex",
+      price: 200,
+      service: 15,
+      available: 8,
+    },
+    {
+      roomtype: "Ultra Dulex",
+      price: 300,
+      service: 30,
+      available: 5,
+    },
+    {
+      roomtype: "Prime Suite",
+      price: 500,
+      service: 40,
+      available: 3,
+    },
+    {
+      roomtype: "Elite Suite",
+      price: 800,
+      service: 50,
+      available: 3,
+    },
+    {
+      roomtype: "Executive Suite",
+      price: 1000,
+      service: 100,
+      available: 5,
+    },
+  ];
+
+  const [booking, setbooking] = useState({
+    availability: 0,
+    roomType: "",
+    roomFair: 0,
+    roomCount: 1,
+    serviceCost: 0,
+    totalDays: 1,
+    totalCost: 0,
+    overallCost:0
+  });
+
+  useEffect(() => {
+    
+    // wright initial api muu!!!
+    
+  }, []);
+
+  let FairCalculate = () => {
+    try {
+      // wright api call here muu!!!
+      alert(`Totally Booked ${booking.roomCount} ${booking.roomType} for ${booking.totalDays} days for the total fair of  ₹ ${booking.overallCost}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(booking);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h2 className="p-5 text-2xl">Booking Page</h2>
+        <hr />
+        <label>Room Type:</label>
+        <div className="p-2 w-3/12 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black">
+          <select
+            value={booking.roomType}
+            onChange={(e) => {
+              datum.map((room) => {
+                if (room.roomtype === e.target.value)
+                  var updatedvalue = {
+                    roomType: e.target.value,
+                    roomFair: room.price,
+                    serviceCost: room.service,
+                    availability: room.available,
+                    totalCost: room.price + room.service,
+                    overallCost:((room.price + room.service)* booking.roomCount)*booking.totalDays,
+                  };
+                setbooking((booking) => ({ ...booking, ...updatedvalue }));
+              });
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <option> select any room</option>
+            {datum.map((room) => {
+              return (
+                <option value={room.roomtype}>
+                  {room.roomtype}:{room.price}/day
+                </option>
+              );
+            })}
+          </select>
         </div>
+        <div>*Excluding Service Cost and Tax</div>
+        <div className="w-3/12 flex flex-row items-center justify-center py-2 ">
+          <div className="flex flex-row items-center justify-between p-2 mr-1 w-6/12 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black">
+            <button onClick={()=>{setbooking((booking) => ({ ...booking, roomCount:booking.roomCount-- }))}}>-</button>
+            <div>Rooms: {booking.roomCount}</div>
+            <button onClick={()=>{setbooking((booking) => ({ ...booking, roomCount:booking.roomCount++ }))}}>+</button>
+          </div>
+          <div className="flex flex-row items-center justify-between p-2 mr-1 w-6/12 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black">
+            <button onClick={()=>{setbooking((booking) => ({ ...booking, totalDays:booking.totalDays-- }))}}>-</button>
+            <div>Days: {booking.totalDays}</div>
+            <button onClick={()=>{setbooking((booking) => ({ ...booking, totalDays:booking.totalDays++ }))}}>+</button>
+          </div>
+        </div>
+
+        <div className="w-3/12 py-2">
+          <h2 className="px-5 text-2xl">Booking Details</h2>
+          <diV>Fair: ₹ {booking.roomFair}/day</diV>
+          <diV>Service Charge: ₹ {booking.serviceCost}/day</diV>
+          <diV>Total Rooms:{booking.roomCount}</diV>
+          <diV>Total Days:{booking.totalDays}</diV>
+          <diV>Total Cost: ₹ {booking.totalCost}</diV>
+          <diV>Overall Cost:  ₹ {booking.overallCost}</diV>
+        </div>
+        <button
+          onClick={FairCalculate}
+          className="p-2 w-3/12 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        >
+          Book Now
+        </button>
+        <div> Having Issue in Booking?</div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
